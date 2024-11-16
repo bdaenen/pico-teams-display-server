56263a6a0331603181be49719154b08ad5dd5af0
Benno Daenen <benno.daenen@cegeka.com>
2024-11-16 13:19:29 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+import { SerialPort } from 'serialport'
+import { DelimiterParser } from '@serialport/parser-delimiter'
+import enquirer from 'enquirer';
+const { Select, Scale } = enquirer
+
+const port = await SerialPort.list().then(res => res.find(p => p.vendorId?.toLowerCase() == '2e8a'))
+
+if (!port) {
+    console.error('Failed to find Pico. Exiting...')
+    process.exit()
+}
+
+console.log('Connected to pico on', port.path)
+
+const serialport = new SerialPort({ path: port.path, baudRate: 9600, parity: 'even', stopBits: 1 })
+const parser = serialport.pipe(new DelimiterParser({ delimiter: '\n' }))
+
+parser.on('data', (d) => {
+    console.log(`pico: ${d.toString()}`);
+})
+
+async function promptAction() {
+    const action = await (new Select({
+        message: 'What do you want to do?',
+        choices: ['Set status', 'Set brightness']
+      })).run()
+    
+    if (action === 'Set status') {
+        const status = await (new Select({
+            message: 'Choose the new status',
+            choices: ['available', 'busy', 'dnd', 'away', 'out of office', 'clear']
+          })).run()
+          serialport.write(`status:${status}\n`);
+    }
+    else if (action === 'Set brightness') {
+        const scale = await (new Scale({
+            message: 'Select a brightness',
+            scale: [
+                { name: '1', message: 'Dimmest' },
+                { name: '2', message: 'Dim' },
+                { name: '3', message: 'Normal' },
+                { name: '4', message: 'Bright' },
+                { name: '5', message: 'Brightest' }
+              ],
+              choices: [
+                {
+                  name: '',
+                  message: 'brightness'
+                }],
+          })).run()
+          const finalBrightness = (Number(scale.brightness) + 1) /10 * 2
+          serialport.write(`brightness:${finalBrightness}\n`);
+    }
+    promptAction()
+}
+
+promptAction()
+
+
+
+// serialport.write('status:clear\n');
